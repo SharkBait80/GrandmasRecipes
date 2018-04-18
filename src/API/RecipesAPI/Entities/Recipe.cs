94d55db6b02a5f14e32a3871837f1eaee8af94bb
Unknown <elginlam@gmail.com>
2018-04-18 10:10:53 +1000

    Moved code files, added Powerpoint

---
+++ src/API/RecipesAPI/Entities/Recipe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Entities
+{
+    [DataContract]
+    public class Recipe
+    {
+        [DataMember]
+        public long Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string RecipeText { get; set; }
+
+        [DataMember]
+        public string ImagePath { get; set; }
+    }
+}

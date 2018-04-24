5584e3e79196ca695853f1d7c145a3a330f4c083
Unknown <elginlam@gmail.com>
2018-04-25 09:38:42 +1000

    Removed hardcoded conn string

--- src/API/RecipesAPI/DataLayer/RecipesDbContext.cs
+++ src/API/RecipesAPI/DataLayer/RecipesDbContext.cs
@@ -12,9 +12,33 @@ namespace DataLayer
 
         public DbSet<Recipe> Recipes { get; set; }
 
+        static string _ConnString;
+
+        static string ConnString
+        {
+            get{
+                if (!string.IsNullOrEmpty(_ConnString))
+                    return _ConnString;
+                else
+                {
+                    // Contact the SSM parameter store to get the values
+                    Amazon.SimpleSystemsManagement.AmazonSimpleSystemsManagementClient simpleSystemsManagementClient = new Amazon.SimpleSystemsManagement.AmazonSimpleSystemsManagementClient();
+
+                    var request = new Amazon.SimpleSystemsManagement.Model.GetParametersRequest();
+                    request.Names = new List<string>();
+                    request.Names.Add("/Dev/RDS/RecipesDb/ConnString");
+                    var response=simpleSystemsManagementClient.GetParametersAsync(request).Result;
+                    if (response!=null && response.Parameters!= null && response.Parameters.Count > 0)
+                        return response.Parameters[0].Value;
+                    else
+                        return null;
+                }
+            }
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseMySQL("server=recipesdb.chzseohsuonx.ap-southeast-2.rds.amazonaws.com;port=3306;database=recipes;uid=dbUser;password=Wh2V0pmDj6GqjthYz42o");
+            optionsBuilder.UseMySQL(ConnString);
             base.OnConfiguring(optionsBuilder);
         }
 
